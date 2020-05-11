8d377cbaccb42795f1a26dd635b84753376b1a23
ezramali <ezhillarasan.ramalingham@tatacommunications.com>
2020-05-11 13:17:59 +0530

    final version

--- airflow-python3.6/entrypoint.d/010-entrypoint-airflow.sh
+++ airflow-python3.6/entrypoint.d/010-entrypoint-airflow.sh
@@ -1,6 +1,7 @@
 # Airflow
 export AIRFLOW_HOME="#AIRFLOW_HOME#"
 export FERNET_KEY="#FERNET_KEY#"
-export PATH=$PATH:/usr/local/bin:/usr/local/sbin:/sbin:/bin:/usr/sbin:/usr/bin:~/bin
+export PATH=$PATH:/usr/local/bin:/usr/local/sbin:/sbin:/bin:/usr/sbin:/usr/bin:~/bin:~/.local/bin:$AIRFLOW_HOME
+export AIRFLOW_CONFIG=$AIRFLOW_HOME/airflow.cfg
 # Java
 export JAVA_HOME=/usr/lib/jvm/jre
