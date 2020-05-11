3dd6eb61c7b4e56ba938eca18cd3bf477fe5970d
ezramali <ezhillarasan.ramalingham@tatacommunications.com>
2020-05-11 10:35:42 +0530

    correction_3.6 docker

--- airflow-python3.6/entrypoint.d/010-entrypoint-airflow.sh
+++ airflow-python3.6/entrypoint.d/010-entrypoint-airflow.sh
@@ -1,6 +1,6 @@
 # Airflow
 export AIRFLOW_HOME="#AIRFLOW_HOME#"
 export FERNET_KEY="#FERNET_KEY#"
-
+export PATH=$PATH:/usr/local/bin:/usr/local/sbin:/sbin:/bin:/usr/sbin:/usr/bin:~/bin
 # Java
 export JAVA_HOME=/usr/lib/jvm/jre
